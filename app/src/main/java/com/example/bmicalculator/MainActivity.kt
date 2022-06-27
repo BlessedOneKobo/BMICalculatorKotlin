ebe6cfd83eba0593be3670c5d31b6b5a96ba07ad
BlessedOneKobo <59095867+BlessedOneKobo@users.noreply.github.com>
2022-06-27 16:28:42 +0100

    feat: implement calculate button click handler

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.bmicalculator
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import com.example.bmicalculator.const.BmiConstants.MAX_AGE
+import com.example.bmicalculator.const.BmiConstants.MIN_AGE
+import com.example.bmicalculator.const.BmiConstants.SNACKBAR_DURATION
 import com.example.bmicalculator.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -22,7 +28,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateBmi() {
+        hideKeyboard()
 
+        val age: Int? = binding.ageInputEditText.text.toString().toIntOrNull()
+        if (age == null || age < MIN_AGE || age > MAX_AGE) {
+            Snackbar.make(binding.root, "Age must be between $MIN_AGE and $MAX_AGE", SNACKBAR_DURATION).show()
+            return
+        }
+
+        val height: Double? = binding.heightInputEditText.text.toString().toDoubleOrNull()
+        if (height == null || height == 0.0) {
+            Snackbar.make(binding.root, "Height is required", SNACKBAR_DURATION).show()
+            return
+        }
+
+        val weight: Double? = binding.weightInputEditText.text.toString().toDoubleOrNull()
+        if (weight == null || weight == 0.0) {
+            Snackbar.make(binding.root, "Weight is required", SNACKBAR_DURATION).show()
+            return
+        }
+
+        val heightInMeters = height / 100.0
+        val bmi: Double = weight / (heightInMeters * heightInMeters)
+        Snackbar.make(binding.root, "Your BMI is $bmi", SNACKBAR_DURATION).show()
     }
 
     private fun clearInputs() {
@@ -31,5 +59,10 @@ class MainActivity : AppCompatActivity() {
         binding.heightInputEditText.setText("")
         binding.weightInputEditText.setText("")
         binding.ageInputEditText.requestFocus()
+    }
+
+    private fun hideKeyboard() {
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(binding.root.windowToken, 0)
     }
 }
---
+++ app/src/main/java/com/example/bmicalculator/const/BmiConstants.kt
@@ -0,0 +1,7 @@
+package com.example.bmicalculator.const
+
+object BmiConstants {
+    const val MIN_AGE: Int = 18
+    const val MAX_AGE: Int = 120
+    const val SNACKBAR_DURATION: Int = 2500
+}
