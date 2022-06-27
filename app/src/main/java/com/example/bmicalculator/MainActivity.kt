7957feee93f673884f6e788d3a174b4c4423368f
BlessedOneKobo <59095867+BlessedOneKobo@users.noreply.github.com>
2022-06-27 15:59:07 +0100

    feat: implement clear button click handler

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bmicalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener {
+            calculateBmi()
+        }
+
+        binding.clearButton.setOnClickListener {
+            clearInputs()
+        }
+    }
+
+    private fun calculateBmi() {
+
+    }
+
+    private fun clearInputs() {
+        binding.ageInputEditText.setText("")
+        binding.maleGenderRadioButton.performClick()
+        binding.heightInputEditText.setText("")
+        binding.weightInputEditText.setText("")
+        binding.ageInputEditText.requestFocus()
     }
 }
